41dcba3e01e0c8bdb0c91c833c690ce6f6df0b38
Cindy Liang <cindy.liang69@gmail.com>
2026-02-24 14:16:14 -0800

    made changes to agentDeployment so that it evaluates over a set number of episodes, however it will not visualize unless it is done in an environment that supports gui

--- snake/agentDeployment.py
+++ snake/agentDeployment.py
@@ -1,30 +1,40 @@
+import numpy as np
 import gymnasium as gym
 from stable_baselines3 import PPO
 from rlEnvironment import snakeRLEnvironment
 
 def main():
-    newEnvironment = snakeRLEnvironment(render_mode = "human")
-    model = PPO.load('ppo_snake')
+    env = snakeRLEnvironment(render_mode = "human")
+    model = PPO.load('ppo_snake.zip')
 
-    obs, info = newEnvironment.reset()
+    obs, info = env.reset()
 
-    while True:
-        action, _states = model.predict(obs)
-    
-        observation, reward, terminated, truncated, info = newEnvironment.step(action.item())
-        newEnvironment.render()
+    n_eval_episodes = 50 
+    all_rewards = []
+    all_lengths = []
+
+    for ep in range(n_eval_episodes):
+        obs, info = env.reset()
+        done = False
+        ep_reward = 0
+        ep_length = 0
+
+        while not done:
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = env.step(action.item())
+            done = terminated or truncated
+            ep_reward += reward
+            ep_length += 1
+            env.render() 
+
+        all_rewards.append(ep_reward)
+        all_lengths.append(ep_length)
+
+    print(f"Average Reward over {n_eval_episodes} episodes: {np.mean(all_rewards):.2f}")
+    print(f"Average Episode Length over {n_eval_episodes} episodes: {np.mean(all_lengths):.1f}")
+
+    env.close()
+
 
 if __name__ == '__main__':
-    # set_environment = snakeRLEnvironment()
-    # obs, info = set_environment.reset()
-    
-    # for i in range(50):
-    #     current_action = set_environment.action_space.sample()
-    #     print("CURRENT ACTION", current_action)
-    #     observation, reward, terminated, truncated, info = set_environment.step(current_action)
-    #     print("REWARD", reward)
-
-    #     if terminated:
-    #         print("RESET")
-    #         obs, info = set_environment.reset()
     main()
