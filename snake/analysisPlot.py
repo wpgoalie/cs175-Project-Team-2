dbcc27069591af4c8752f85ec58572db1972f808
Sarkis Vincent Dzhigneyan <sdzhigne@hpc3-19-14.local>
2026-02-24 21:01:18 -0800

    Finished up the evaluation section.

--- snake/analysisPlot.py
+++ snake/analysisPlot.py
@@ -1,10 +1,19 @@
 import csv
+import pandas as pd
+
+
 
 def main():
-    with open('data/misc/eval_metrics.csv', mode ='r') as file:
-        csvFile = csv.reader(file)
-        for lines in csvFile:
-            print(lines)
+    # with open('data/misc/eval_metrics.csv', mode ='r') as file:
+    #     csvFile = csv.reader(file)
+    #     for lines in csvFile:
+    #         print(f'{lines[0]} | {lines[1]} | ')
+
+    # Read CSV
+    df = pd.read_csv("data/misc/eval_metrics.csv")
+    
+    # Convert to Markdown table
+    print(df.to_markdown(index=False))
 
 if __name__ == '__main__':
     main()
